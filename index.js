07a47202eefc221646f95182269e9c5bc13ca9d9
rkan75 <42704310+rkan75@users.noreply.github.com>
2023-05-14 16:35:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
+ 
   const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const nameToGreet = core.getInput('girRepoURL');
+  console.log(`Hello ${nameToGreet}!`);
+  const nameToGreet = core.getInput('gitRepoBranch');
+  console.log(`Hello ${nameToGreet}!`);
+  const nameToGreet = core.getInput('rootInfraYmlFile');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  //const payload = JSON.stringify(github.context.payload, undefined, 2)
+  //console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
